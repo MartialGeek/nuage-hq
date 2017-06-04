3c2f79f73b470b59082c7b11c147f391967013fa
Martial Saunois <saunois.martial>
2017-06-04 23:57:40 +0200

    Finish the 19th lesson of the Redux course

--- web/src/main.js
+++ web/src/main.js
@@ -1,48 +1,178 @@
 'use strict';
 
-import expect from 'expect';
-import { createStore } from 'redux';
-import React from 'react';
+import { createStore, combineReducers } from 'redux';
 import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id === action.id) {
+        return {
+          ...state,
+          completed: !state.completed
+        };
+      }
+
+      return state;
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+};
+
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+const store = createStore(todoApp);
+
+const FilterLink = ({ filter, currentFilter, children }) => {
+  if (filter === currentFilter) {
+    return (
+      <span>{children}</span>
+    );
+  }
+  
+  return (
+    <a href='#'
+      onClick={e => {
+        e.preventDefault();
+        store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter
+        });
+      }}
+    >
+      {children}
+    </a>
+  );
+};
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed);
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed);
+  }
+};
+
+let todoId = 0;
+
+class TodoApp extends Component {
+  render() {
+    const {
+      todos,
+      visibilityFilter
+    } = this.props;
+
+    const visibleTodos = getVisibleTodos(
+      todos,
+      visibilityFilter
+    );
+
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node;
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            id: todoId++,
+            text: this.input.value
+          });
+          this.input.value = '';
+        }}>
+          Add todo
+        </button>
+        <ul>
+          {visibleTodos.map(todo =>
+            <li
+              key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                });
+              }}
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none'
+              }}
+            >{todo.text}</li>
+          )}
+        </ul>
+        <p>
+          Show:
+          {' '}
+          <FilterLink
+            filter='SHOW_ALL'
+            currentFilter={visibilityFilter}
+          >
+            All
+          </FilterLink>
+          {' '}
+          <FilterLink
+            filter='SHOW_ACTIVE'
+            currentFilter={visibilityFilter}
+          >
+            Active
+          </FilterLink>
+          {' '}
+          <FilterLink
+            filter='SHOW_COMPLETED'
+            currentFilter={visibilityFilter}
+          >
+            Completed
+          </FilterLink>
+        </p>
+      </div>
+    );
+  }
 }
 
-const Counter = ({
-    value,
-    onIncrement,
-    onDecrement
-}) => (
-    <div>
-        <h1>{value}</h1>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
-    </div>
-);
-
-const store = createStore(counter);
-
 const render = () => {
-    ReactDOM.render(
-        <Counter
-            value={store.getState()}
-            onIncrement={() =>
-                store.dispatch({ type: 'INCREMENT' })
-            }
-            onDecrement={() =>
-                store.dispatch({ type: 'DECREMENT' })
-            }
-            />,
-        document.getElementById('root')
-    );
+  console.log(store.getState());
+  ReactDOM.render(
+    <TodoApp
+      {...store.getState()}
+    />,
+    document.getElementById('root')
+  );
 };
 
 store.subscribe(render);
