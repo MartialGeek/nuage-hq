41f3354e2e3236883dac8bfd68cfd2aade2dbf7b
Martial Saunois <martial@saunois.net>
2017-06-05 14:54:57 +0200

    Create the Todo and TodoList components

--- web/src/main.js
+++ web/src/main.js
@@ -91,6 +91,33 @@ const getVisibleTodos = (todos, filter) => {
   }
 };
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style={{ textDecoration: completed ? 'line-through' : 'none' }}
+  >{text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
 let todoId = 0;
 
 class TodoApp extends Component {
@@ -126,22 +153,15 @@ class TodoApp extends Component {
             Add todo
           </button>
         </form>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li
-              key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none'
-              }}
-            >{todo.text}</li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          }
+        />
         <p>
           Show:
           {' '}
