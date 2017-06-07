a140ff1b0b9766f86b65ac14ddbd235542c017f0
Martial Saunois <martial@saunois.net>
2017-06-07 21:27:58 +0200

    Filtering Redux state with React router params

--- web/src/reducers/combinedReducers.js
+++ web/src/reducers/combinedReducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import todos from './todos';
-import visibilityFilter from './visibilityFilter';
 
 const combinedReducers = combineReducers({
-  todos,
-  visibilityFilter
+  todos
 });
 
 export default combinedReducers;
